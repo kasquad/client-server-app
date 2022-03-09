27fb3e3a2c1cedfd876865f96d228b4e692c1c18
Radmir <RaRDautov@stud.kpfu.ru>
2022-03-09 19:53:52 +0300

    simple realisation

---
+++ csharp-client/csharp-client/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using Newtonsoft.Json;
+
+namespace csharp_client
+{
+    class Program
+    {
+        class Message
+        {
+            private string text;
+            public string Text { get { return text; } }
+            public Message(string text)
+            {
+                this.text = text;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+           
+            using (WebSocket ws = new WebSocket("ws://localhost:4040/send/message"))
+            {
+                ws.OnMessage += Ws_OnMessage;
+                ws.Connect();
+                Console.WriteLine("Connected to server");
+                string data = string.Empty;
+                while(true)
+                {
+
+                    Console.WriteLine("Enter message (if u want to disconnect type 'disconnect') : ");
+                    data = Console.ReadLine();
+                    if (data == "disconnect")
+                        break;
+                    Message msg = new Message(data);
+                    ws.Send(JsonConvert.SerializeObject(msg));
+                }
+                ws.Close();
+            }
+            
+        }
+
+        private static void Ws_OnMessage(object sender, MessageEventArgs e)
+        {
+            try
+            {
+                 Console.WriteLine("Server taked message");
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
---
+++ csharp-server/csharp-server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using Newtonsoft.Json;
+
+
+namespace csharp_server
+{
+    class Message
+    {
+        private string text;
+        public string Text { get { return text; } }
+        public Message(string text)
+        {
+            this.text = text;
+        }
+    }
+
+
+    public class MessageService : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Message msg = JsonConvert.DeserializeObject<Message>(e.Data);
+            Console.WriteLine("Received from client: " + msg.Text);
+
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+  
+                WebSocketServer wssv = new WebSocketServer("ws://localhost:4040");
+                wssv.AddWebSocketService<MessageService>("/send/message");
+                wssv.Start();
+                Console.WriteLine("Server started on ws://localhost:4040/send/message");
+
+                do
+                {
+                    Console.WriteLine("If u want to stop  type 'stop'");
+                } while (Console.ReadLine() != "stop");
+
+                wssv.Stop();
+          
+        }
+    }
+}
